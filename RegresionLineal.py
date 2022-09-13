4ee93ae53d540865b3fee2d61c3446ac7cc30bcc
a01750147 <a01750147@itesm.mx>
2022-09-13 16:35:23 -0500

    Update RegresionLineal.py

--- RegresionLineal.py
+++ RegresionLineal.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import SGDRegressor
+import matplotlib.pyplot as plt 
 
 def modelo(dt):
     dt = dt.rename(columns = {'1991': 'uno', '1992': 'dos',  '1993':'tres', '1994':'cuatro'})
     x = dt.drop(columns = ['cuatro'])
     y = dt['cuatro']
-    mod = SGDRegressor(loss = 'squared_loss',alpha = 0.001, max_iter = 100)
+    mod = SGDRegressor(loss = 'squared_loss',alpha = 0.001, max_iter = 10000)
     mod = mod.fit(x,y)
     print("Parámetros: ", mod.coef_)
     print("\n")
@@ -27,6 +28,8 @@ X = X.drop(columns = ['cuatro'])
 
 test = mod.predict(X)
 print("R2:", mod.score(X,y))
+print("Bias: ", abs(test - y).mean())
+print("Varianza: ", np.var(test))
 print("\n")
 
 
@@ -44,6 +47,8 @@ X = X.drop(columns = ['cuatro'])
 
 test = mod.predict(X)
 print("R2:", mod.score(X,y))
+print("Bias: ", (test - y).mean())
+print("Varianza: ", np.var(test))
 print("\n")
 
 #Predicciones:
@@ -56,5 +61,16 @@ dt = np.array([0.1, 0.05, 0.25])
 dt = pd.DataFrame([dt], columns = ['uno', 'dos', 'tres'])
 print(mod.predict(dt))
 
+
+#Gráfica.
+x_ax = range(len(test))
+plt.plot(x_ax, y, linewidth=1, label="Y real")
+plt.plot(x_ax, test, linewidth=1.1, label="Predicción")
+plt.title("Y real vs Predicción")
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.legend(loc='best',fancybox=True, shadow=True)
+plt.grid(True)
+plt.show() 
 
 
